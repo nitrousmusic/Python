b8935a5b4add46dfab1b5cddb663e7d003642e3e
NitrousMusic <37781657+nitrousmusic@users.noreply.github.com>
2022-10-11 15:26:20 -0400

    MadLibs Game
    "Wikipedia Definition!"
    Mad Libs is a phrasal template word game created by Leonard Stern[1][2] and Roger Price.[3] It consists of one player prompting others for a list of words to substitute for blanks in a story before reading aloud. The game is frequently played as a party game or as a pastime.
    
    The game was invented in the United States, and more than 110 million copies of Mad Libs books have been sold since the series was first published in 1958.[3]
    
    https://en.wikipedia.org/wiki/Mad_Libs

---
+++ bigGame.py
@@ -0,0 +1,34 @@
+#Write a program that plays a MadLib game witht he suer
+
+# The program should ask the user to enter the following words
+
+print("Enter the following words")
+
+
+proNoun = input("Enter a Plural Noun: ")
+firstName = input("What's your name? ")
+noun1 = input("Enter a Noun: ")
+lastName = input("Enter last name ")
+proNoun2 = input("Enter a second Plural Noun ")
+
+place1 = input("Name a place: ")
+proNoun3 = input("Enter a 3rd Plural Noun: ")
+place2 = input("Enter another place: ")
+proNoun4 = input("Enter a 4rth Plural Noun: ")
+
+noun2 = input("write another Noun: ")
+adjective = input("Write an Adjective: ")
+adjective2 = input("Write another Adjective2: ")
+verb = input("Wirte a Verb: ")
+adjective3 = input("write Adjective3: ")
+
+print("Hello there, sports " + proNoun + "!")
+print("This is " + firstName + ", talking to you from the press " + noun1)
+print("in " + lastName + " Stadium, where 57,000 cheering " + proNoun2 + ".")
+
+print("Have gathered to watch " + place1, proNoun3 + ".")
+print("take on the " + place2, proNoun4 + ".")
+
+print("Even though the " + noun2 + " is shinning, it's a " + adjective2 + " 20s,. ")
+print("We'll be back for the opening " + verb + " -off after a few words from our " + adjective3 + " sponsors.")
+
